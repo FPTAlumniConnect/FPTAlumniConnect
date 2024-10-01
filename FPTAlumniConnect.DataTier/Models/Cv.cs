8deeeb0c7d02293e835ffc5f001ed5aa4fe1b08a
kirizenju <quang.com.vn1911@gmail.com>
2024-10-01 14:55:33 +0700

    models

---
+++ FPTAlumniConnect.DataTier/Models/Cv.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FPTAlumniConnect.DataTier.Models;
+
+public partial class Cv
+{
+    public int Id { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string Type { get; set; } = null!;
+
+    public string Cv1 { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public string? UpdatedBy { get; set; }
+
+    public virtual ICollection<JobApplication> JobApplications { get; set; } = new List<JobApplication>();
+
+    public virtual User? User { get; set; }
+}
